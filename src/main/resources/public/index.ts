ad44bdcc14ccccb939469f719beb4cd37315a986
Matthias Fuchs <matthias.fuchs@kps.com>
2020-01-23 20:50:55 +0100

    added material design light

--- src/main/resources/public/index.ts
+++ src/main/resources/public/index.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import "vue-class-component/hooks"; // import hooks type to enable auto-complete
+// import "../../../../node_modules/material-design-lite/material.min.css";
 import App from "../../typescript/App.vue";
 import UILabel from "../../typescript/components/UILabel.vue";
 
@@ -7,6 +8,7 @@ Vue.config.productionTip = false;
 
 Vue.component("ui-label", UILabel);
 
-new Vue({
+const root = new Vue({
+    el: "#root",
     render: (createElement) => createElement(App),
-}).$mount("#root");
+});
--- src/main/typescript/App.vue
+++ src/main/typescript/App.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>
-        <nav>
-            <h1>{{title}}</h1>
-        </nav>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header">
+            <div class="mdl-layout__header-row">
+                <!-- Title -->
+                <span class="mdl-layout-title">{{title}}</span>
 
-        <section id="content">
-            <ui-label text="test label"></ui-label>
-        </section>
+                <!-- Add spacer, to align navigation to the right -->
+                <div class="mdl-layout-spacer"></div>
+
+                <!-- Navigation. We hide it in small screens. -->
+                <nav class="mdl-navigation mdl-layout--large-screen-only">
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                </nav>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Title</span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+
+        <main class="mdl-layout__content">
+            <div class="page-content">
+                <section id="content">
+                    <ui-label text="test label"></ui-label>
+                </section>
+            </div>
+        </main>
     </div>
+
 </template>
 
 <script lang="ts">
@@ -16,11 +44,5 @@
 </script>
 
 <style>
-    nav {
-        background-color: purple;
-        color: white;
-        padding: 1px;
-        padding-left: 30px;
-    }
 
 </style>
